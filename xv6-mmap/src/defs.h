b2cd1f1e9a1cdb960008a906a668e7b9333b9b30
taisun93 <taisun93@gmail.com>
2023-04-02 15:39:34 -0500

    a

--- xv6-mmap/src/defs.h
+++ xv6-mmap/src/defs.h
@@ -164,6 +164,9 @@ int             fetchint(uint, int*);
 int             fetchstr(uint, char**);
 void            syscall(void);
 
+// sysfile.c
+int             fdalloc(struct file*);
+
 // timer.c
 void            timerinit(void);
 
@@ -193,6 +196,6 @@ void            switchuvm(struct proc*);
 void            switchkvm(void);
 int             copyout(pde_t*, uint, void*, uint);
 void            clearpteu(pde_t *pgdir, char *uva);
-int             fdalloc(struct file*);
+
 // number of elements in fixed-size array
 #define NELEM(x) (sizeof(x)/sizeof((x)[0]))
