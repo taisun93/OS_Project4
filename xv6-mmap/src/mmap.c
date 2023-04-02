df9da98429e1e9a00d6f8086a2304146ee121ae0
taisun93 <taisun93@gmail.com>
2023-04-02 15:46:49 -0500

    reformat

--- xv6-mmap/src/mmap.c
+++ xv6-mmap/src/mmap.c
@@ -13,20 +13,20 @@
 #define PROT_WRITE 1
 #define MMAPBASE 0x40000000
 
-
-int
-fdalloc(struct file *f)
+int fdalloc(struct file *f)
 {
-  int fd;
-  struct proc *curproc = myproc();
+    int fd;
+    struct proc *curproc = myproc();
 
-  for(fd = 0; fd < NOFILE; fd++){
-    if(curproc->ofile[fd] == 0){
-      curproc->ofile[fd] = f;
-      return fd;
+    for (fd = 0; fd < NOFILE; fd++)
+    {
+        if (curproc->ofile[fd] == 0)
+        {
+            curproc->ofile[fd] = f;
+            return fd;
+        }
     }
-  }
-  return -1;
+    return -1;
 }
 
 void *mmap(void *addr, uint length, int prot, int flags, int fd, int offset)
@@ -74,15 +74,15 @@ void *mmap(void *addr, uint length, int prot, int flags, int fd, int offset)
     {
         if (fd > -1)
         {
-          if((fd=fdalloc(p->ofile[fd])) < 0)
-            return (void*)-1;
-          filedup(p->ofile[fd]);
-          r->fd = fd;
+            if ((fd = fdalloc(p->ofile[fd])) < 0)
+                return (void *)-1;
+            filedup(p->ofile[fd]);
+            r->fd = fd;
         }
         else
         {
-        kmfree(r);
-        return (void *)-1;
+            kmfree(r);
+            return (void *)-1;
         }
     }
 
@@ -127,86 +127,86 @@ void *mmap(void *addr, uint length, int prot, int flags, int fd, int offset)
 
 int munmap(void *addr, uint length)
 {
-  // Sanity check on addr and length
-  if (addr == (void*)KERNBASE || addr > (void*)KERNBASE || length < 1)
-  {
+    // Sanity check on addr and length
+    if (addr == (void *)KERNBASE || addr > (void *)KERNBASE || length < 1)
+    {
+        return -1;
+    }
+
+    struct proc *p = myproc();
+
+    // If nothing has been allocated, there is nothing to munmap
+    if (p->nregions == 0)
+    {
+        return -1;
+    }
+
+    // Travese our mmap dll to see if address and length are valid
+    mmapped_region *prev = p->region_head;
+    mmapped_region *next = p->region_head->next;
+    int size = 0;
+
+    // Check the head
+    if (p->region_head->start_addr == addr && p->region_head->length == length)
+    {
+        /*deallocate the memory from the current process*/
+        p->sz = deallocuvm(p->pgdir, p->sz, p->sz - length);
+        switchuvm(p);
+        p->nregions--;
+
+        // close the file we were mapping to
+        if (p->region_head->region_type == MAP_FILE)
+        {
+            fileclose(p->ofile[p->region_head->fd]);
+            p->ofile[p->region_head->fd] = 0;
+        }
+
+        // if(p->region_head->next != 0)
+        // {
+        /* Calls to kmfree were changing the node->next's length value
+         * in the linked-list. This is a hacky fix, but I don't know
+         * what is really causing that problem... */
+        size = p->region_head->next->length;
+        ll_delete(p->region_head, 0);
+        p->region_head->length = size;
+        // }
+        // else
+        // {
+        //   ll_delete(p->region_head, 0);
+        // }
+
+        /*return success*/
+        return 0;
+    }
+
+    while (next != 0)
+    {
+        if (next->start_addr == addr && next->length == length)
+        {
+            /*deallocate the memory from the current process*/
+            p->sz = deallocuvm(p->pgdir, p->sz, p->sz - length);
+            switchuvm(p);
+            p->nregions--;
+
+            // close the file we were mapping to
+            if (next->region_type == MAP_FILE)
+            {
+                fileclose(p->ofile[next->fd]);
+                p->ofile[next->fd] = 0;
+            }
+
+            /*remove the node from our ll*/
+            size = next->next->length;
+            ll_delete(next, prev);
+            prev->next->length = size;
+
+            /*return success*/
+            return 0;
+        }
+        prev = next;
+        next = prev->next;
+    }
+
+    // if there was no match, return -1
     return -1;
-  }
-
-  struct proc *p = myproc();
-
-  // If nothing has been allocated, there is nothing to munmap
-  if (p->nregions == 0)
-  {
-    return -1;
-  }
-
-  // Travese our mmap dll to see if address and length are valid
-  mmapped_region *prev = p->region_head;
-  mmapped_region *next = p->region_head->next;
-  int size = 0;
-
-  // Check the head
-  if (p->region_head->start_addr == addr && p->region_head->length == length)
-  {
-    /*deallocate the memory from the current process*/
-    p->sz = deallocuvm(p->pgdir, p->sz, p->sz - length);
-    switchuvm(p);
-    p->nregions--;  
-
-    // close the file we were mapping to
-    if(p->region_head->region_type == MAP_FILE)
-    {
-      fileclose(p->ofile[p->region_head->fd]);
-      p->ofile[p->region_head->fd] = 0;
-    }
-
-    if(p->region_head->next != 0)
-    {
-      /* Calls to kmfree were changing the node->next's length value
-       * in the linked-list. This is a hacky fix, but I don't know
-       * what is really causing that problem... */
-      size = p->region_head->next->length;
-      ll_delete(p->region_head, 0);
-      p->region_head->length = size;
-    }
-    else
-    {
-      ll_delete(p->region_head, 0);
-    }
-
-    /*return success*/
-    return 0;
-  }
-
-  while(next != 0)
-  {
-    if (next->start_addr == addr && next->length == length)
-    {
-      /*deallocate the memory from the current process*/
-      p->sz = deallocuvm(p->pgdir, p->sz, p->sz - length);
-      switchuvm(p);
-      p->nregions--;  
-      
-      // close the file we were mapping to
-      if(next->region_type == MAP_FILE)
-      {
-        fileclose(p->ofile[next->fd]);
-        p->ofile[next->fd] = 0;
-      }
-
-      /*remove the node from our ll*/
-      size = next->next->length;
-      ll_delete(next, prev);
-      prev->next->length = size;
-      
-      /*return success*/
-      return 0;
-    }
-    prev = next;
-    next = prev->next;
-  }
-
-  // if there was no match, return -1
-  return -1;
 }
