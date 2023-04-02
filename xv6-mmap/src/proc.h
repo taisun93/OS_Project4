39e9dbb76b0b405e5a6e0f76cc26aa81745a03a0
taisun93 <taisun93@gmail.com>
2023-04-02 14:00:00 -0500

    proc.h

--- xv6-mmap/src/proc.h
+++ xv6-mmap/src/proc.h
@@ -1,30 +1,32 @@
 // Per-CPU state
-struct cpu {
-  uchar apicid;                // Local APIC ID
-  struct context *scheduler;   // swtch() here to enter scheduler
-  struct taskstate ts;         // Used by x86 to find stack for interrupt
-  struct segdesc gdt[NSEGS];   // x86 global descriptor table
-  volatile uint started;       // Has the CPU started?
-  int ncli;                    // Depth of pushcli nesting.
-  int intena;                  // Were interrupts enabled before pushcli?
-  struct proc *proc;           // The process running on this cpu or null
+struct cpu
+{
+  uchar apicid;              // Local APIC ID
+  struct context *scheduler; // swtch() here to enter scheduler
+  struct taskstate ts;       // Used by x86 to find stack for interrupt
+  struct segdesc gdt[NSEGS]; // x86 global descriptor table
+  volatile uint started;     // Has the CPU started?
+  int ncli;                  // Depth of pushcli nesting.
+  int intena;                // Were interrupts enabled before pushcli?
+  struct proc *proc;         // The process running on this cpu or null
 };
 
 extern struct cpu cpus[NCPU];
 extern int ncpu;
 
-//PAGEBREAK: 17
-// Saved registers for kernel context switches.
-// Don't need to save all the segment registers (%cs, etc),
-// because they are constant across kernel contexts.
-// Don't need to save %eax, %ecx, %edx, because the
-// x86 convention is that the caller has saved them.
-// Contexts are stored at the bottom of the stack they
-// describe; the stack pointer is the address of the context.
-// The layout of the context matches the layout of the stack in swtch.S
-// at the "Switch stacks" comment. Switch doesn't save eip explicitly,
-// but it is on the stack and allocproc() manipulates it.
-struct context {
+// PAGEBREAK: 17
+//  Saved registers for kernel context switches.
+//  Don't need to save all the segment registers (%cs, etc),
+//  because they are constant across kernel contexts.
+//  Don't need to save %eax, %ecx, %edx, because the
+//  x86 convention is that the caller has saved them.
+//  Contexts are stored at the bottom of the stack they
+//  describe; the stack pointer is the address of the context.
+//  The layout of the context matches the layout of the stack in swtch.S
+//  at the "Switch stacks" comment. Switch doesn't save eip explicitly,
+//  but it is on the stack and allocproc() manipulates it.
+struct context
+{
   uint edi;
   uint esi;
   uint ebx;
@@ -32,12 +34,21 @@ struct context {
   uint eip;
 };
 
-enum procstate { UNUSED, EMBRYO, SLEEPING, RUNNABLE, RUNNING, ZOMBIE };
+enum procstate
+{
+  UNUSED,
+  EMBRYO,
+  SLEEPING,
+  RUNNABLE,
+  RUNNING,
+  ZOMBIE
+};
 
 // Per-process state
-struct proc {
+struct proc
+{
   uint sz;                     // Size of process memory (bytes)
-  pde_t* pgdir;                // Page table
+  pde_t *pgdir;                // Page table
   char *kstack;                // Bottom of kernel stack for this process
   enum procstate state;        // Process state
   int pid;                     // Process ID
@@ -49,6 +60,8 @@ struct proc {
   struct file *ofile[NOFILE];  // Open files
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
+  int nregions;                
+  mmapped_region *region_head; 
 };
 
 // Process memory is laid out contiguously, low addresses first:
@@ -57,16 +70,16 @@ struct proc {
 //   fixed-size stack
 //   expandable heap
 
-typedef struct mmapped_region 
-{ 
-  //Linked-List pointers
-  struct mmapped_region* next;
+typedef struct mmapped_region
+{
+  // Linked-List pointers
+  struct mmapped_region *next;
 
-  //Region Meta-Data:
-  void* start_addr; //starting address for mapped region 
-  uint length;      //length of allocated region
-  int region_type; //anonymous of file-backed
-  int offset;      //offset in a file-backed allocation
-  int fd;          //file descriptor (-1 for anonymous allocation)
-  int prot;        //protection bits for the mapped region (default is read-only)
+  // Region Meta-Data:
+  void *start_addr; // starting address for mapped region
+  uint length;      // length of allocated region
+  int region_type;  // anonymous of file-backed
+  int offset;       // offset in a file-backed allocation
+  int fd;           // file descriptor (-1 for anonymous allocation)
+  int prot;         // protection bits for the mapped region (default is read-only)
 } mmapped_region;
