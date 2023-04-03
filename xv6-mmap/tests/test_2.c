e694f92225927a78835fdf3b970a8293009c6e51
taisun93 <taisun93@gmail.com>
2023-04-03 10:45:51 -0500

    debug

--- xv6-mmap/tests/test_2.c
+++ xv6-mmap/tests/test_2.c
@@ -41,9 +41,11 @@ void test() {
   }
 
   printf(1, "XV6_TEST_OUTPUT : mmap good\n");
-  printf(1, "XV6_TEST_OUTPUT : 12345\n");
+  
   zeroes = malloc(size);
+  printf(1, "XV6_TEST_OUTPUT : past malloc\n");
   memset(zeroes, 0, size);
+  printf(1, "XV6_TEST_OUTPUT : past memset\n");
 
   int ret = memcmp(res, zeroes, size);
   printf(1, "XV6_TEST_OUTPUT : Ret of memcmp %d\n", ret);
