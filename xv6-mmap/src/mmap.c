38713a0f488b46da34ecd79191a6661b0cf8e6cc
taisun93 <taisun93@gmail.com>
2023-04-06 20:28:23 -0500

    a

--- xv6-mmap/src/mmap.c
+++ xv6-mmap/src/mmap.c
@@ -20,7 +20,7 @@ void *mmap(void *addr, int length, int prot, int flags, int fd, int offset)
     uint oldsz = (KERNBASE / 2);
     uint newsz = oldsz + length;
     // Expand process size
-    allocuvm(p->pgdir, PGROUNDUP(oldsz), 100);
+    allocuvm(p->pgdir, PGROUNDUP(oldsz), newsz);
     // allocuvm(p->pgdir, (uint)20000, (uint)23000);
 
     // p->sz = p->sz + length;
