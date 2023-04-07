8b38cbda6fd49b06a3ecd33e767e2ced540fbd95
taisun93 <taisun93@gmail.com>
2023-04-06 21:03:49 -0500

    munmap

--- xv6-mmap/src/mmap.c
+++ xv6-mmap/src/mmap.c
@@ -48,7 +48,6 @@ void *mmap(void *addr, int length, int prot, int flags, int fd, int offset)
 
 int munmap(void *addr, int length)
 {
-    return 1;
     struct proc *p = myproc();
     if ((int)addr % 4096 != 0)
     {
@@ -71,6 +70,9 @@ int munmap(void *addr, int length)
 
         active = active->next;
         counter--;
+        if(active->next==0){
+            return -1;
+        }
     }
 
     return -1;
